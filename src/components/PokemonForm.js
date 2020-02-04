399ae920902929051b41184f411181266c892cc2
Valentine <valentinemaillard1@gmail.com>
2020-02-04 11:12:13 -0500

    pokemon

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,16 +13,28 @@ class PokemonForm extends React.Component {
     }
   }
 
+  handleChange = (event) => {
+    let {name, value} = event.target
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = () => {
+    this.props.createNewPokemon(this.state)
+  }
+
   render() {
     return (
       <div>
         <h3>Add a Pokemon!</h3>
         <Form onSubmit={this.handleSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid label="Name" placeholder="Name" name="name" />
-            <Form.Input fluid label="hp" placeholder="hp" name="hp" />
-            <Form.Input fluid label="Front Image URL" placeholder="url" name="frontUrl" />
-            <Form.Input fluid label="Back Image URL" placeholder="url" name="backUrl" />
+            <Form.Input fluid onChange={this.handleChange} value={this.state.name} label="Name" placeholder="Name" name="name" />
+            <Form.Input fluid onChange={this.handleChange} value={this.state.hp} label="hp" placeholder="hp" name="hp" />
+            <Form.Input fluid onChange={this.handleChange} value={this.state.frontUrl} label="Front Image URL" placeholder="url" name="frontUrl" />
+            <Form.Input fluid onChange={this.handleChange} value={this.state.backUrl} label="Back Image URL" placeholder="url" name="backUrl" />
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
